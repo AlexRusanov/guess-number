63511b1451cf5d9917f2a3910281fe9d76f1ad8a
alexandrrusanov <Darkelf3001>
2020-01-26 16:00:58 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,40 @@ public class Main {
         String name;
         Player player;
         int number;
+        int questionNumber;
+
+        Random random = new Random();
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Let the game begin! Enter your name:");
 
-        while (!scanner.hasNextLine()) {
-            name = scanner.nextLine();
-            player = new Player(name);
+        name = scanner.nextLine();
+        player = new Player(name);
+
+        while (true) {
+            questionNumber = random.nextInt(5);
+            System.out.println(Questions.QUESTIONS[questionNumber][1]);
+
+            do {
+                while (!scanner.hasNextInt()){
+                    System.out.println("It's not a number. Please enter the number!");
+                    scanner.next();
+                }
+
+                number = scanner.nextInt();
+
+                if (number > Integer.parseInt(Questions.QUESTIONS[questionNumber][0])) {
+                    System.out.println("Your number is too big. Please, try again.");
+                } else if (number < Integer.parseInt(Questions.QUESTIONS[questionNumber][0])) {
+                    System.out.println("Your number is too small. Please, try again.");
+                } else {
+                    System.out.println("Congratulations, " + player.getName() + "!");
+                    player.addAnswer(number);
+                    System.out.println("Your numbers:");
+                    player.printAnswers();
+                    break;
+                }
+            } while (number != Integer.parseInt(Questions.QUESTIONS[questionNumber][0]));
         }
-
-        Random random = new Random();
-        System.out.println(Questions.QUESTIONS[random.nextInt()][1]);
-
-
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,36 @@
 public class Player {
 
     private String name;
+    private int[] answers;
+    private int count;
+
+    private final static int DEFAULT_LENGTH = 2;
 
     public Player(String name) {
         this.name = name;
+        this.answers = new int[DEFAULT_LENGTH];
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public void printAnswers() {
+        String answersToPrint = "";
+
+        for (int i = 0; i < count; i++) {
+            answersToPrint = answersToPrint.concat(answers[i] + "  ");
+        }
+
+        System.out.println(answersToPrint);
+    }
+
+    public void addAnswer(int number) {
+        if (answers.length * 0.8 < count) {
+            int[] newArray = new int[answers.length * 2];
+            System.arraycopy(answers, 0, newArray, 0, answers.length);
+            answers = newArray;
+        }
+        answers[count++] = number;
     }
 }
