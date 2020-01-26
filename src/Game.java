bb5086f62e16a3e256c7dd80e0f8ecc258158f22
alexandrrusanov <Darkelf3001>
2020-01-26 16:32:48 +0200

    some fixes

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,51 @@
-public class Game {
+import java.util.Random;
+import java.util.Scanner;
 
+public class Game {
+    private Player player;
+    private int currentQuestion;
+
+    public Game() {
+    }
+
+    public void setCurrentQuestion(int currentQuestion) {
+        this.currentQuestion = currentQuestion;
+    }
+
+    public void play() {
+        Random random = new Random();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Let the game begin! Enter your name:");
+
+        player = new Player(scanner.nextLine());
+
+        int number;
+
+        while (true) {
+            setCurrentQuestion(random.nextInt(5));
+            System.out.println(Questions.QUESTIONS[this.currentQuestion][1]);
+
+            do {
+                while (!scanner.hasNextInt()){
+                    System.out.println("It's not a number. Please enter the number!");
+                    scanner.next();
+                }
+
+                number = scanner.nextInt();
+
+                if (number > Integer.parseInt(Questions.QUESTIONS[this.currentQuestion][0])) {
+                    System.out.println("Your number is too big. Please, try again.");
+                } else if (number < Integer.parseInt(Questions.QUESTIONS[this.currentQuestion][0])) {
+                    System.out.println("Your number is too small. Please, try again.");
+                } else {
+                    System.out.println("Congratulations, " + player.getName() + "!");
+                    player.addAnswer(number);
+                    System.out.println("Your numbers:");
+                    player.printAnswers();
+                    break;
+                }
+            } while (number != Integer.parseInt(Questions.QUESTIONS[this.currentQuestion][0]));
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,6 @@
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        String name;
-        Player player;
-        int number;
-        int questionNumber;
-
-        Random random = new Random();
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Let the game begin! Enter your name:");
-
-        name = scanner.nextLine();
-        player = new Player(name);
-
-        while (true) {
-            questionNumber = random.nextInt(5);
-            System.out.println(Questions.QUESTIONS[questionNumber][1]);
-
-            do {
-                while (!scanner.hasNextInt()){
-                    System.out.println("It's not a number. Please enter the number!");
-                    scanner.next();
-                }
-
-                number = scanner.nextInt();
-
-                if (number > Integer.parseInt(Questions.QUESTIONS[questionNumber][0])) {
-                    System.out.println("Your number is too big. Please, try again.");
-                } else if (number < Integer.parseInt(Questions.QUESTIONS[questionNumber][0])) {
-                    System.out.println("Your number is too small. Please, try again.");
-                } else {
-                    System.out.println("Congratulations, " + player.getName() + "!");
-                    player.addAnswer(number);
-                    System.out.println("Your numbers:");
-                    player.printAnswers();
-                    break;
-                }
-            } while (number != Integer.parseInt(Questions.QUESTIONS[questionNumber][0]));
-        }
+        Game game = new Game();
+        game.play();
     }
 }
